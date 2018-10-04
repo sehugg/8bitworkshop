aa9a829373bb00e1c0f2dc4572519cb19efc1ed0
Steven Hugg <hugg@fasterlight.com>
2018-10-03 21:14:10 -0400

    fix unit tests

--- test/cli/testworker.js
+++ test/cli/testworker.js
@@ -2,7 +2,7 @@
 var assert = require('assert');
 var fs = require('fs');
 var wtu = require('./workertestutils.js');
-var heapdump = require('heapdump');
+//var heapdump = require('heapdump');
 
 // TODO: why memory leak?
 CACHE_WASM_MODULES = false;
