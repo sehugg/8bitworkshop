77123211cf7315ae3984fc6124210769c12cc4af
Steven Hugg <hugg@fasterlight.com>
2019-04-30 16:52:07 -0400

    removed website files from IDE project

--- scripts/sync-intro.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. ./scripts/env.sh
-grep "var VERSION" redir.html
-mkdir -p .tmp
-cp demo.html .tmp/index.html
-rsync -rpilv --chmod=a+rx -e "ssh -p 2222" .tmp/index.html .htaccess redir.html privacy.html css bootstrap images $RSYNC_PATH/
-rm -fr .tmp
-echo "Done."
