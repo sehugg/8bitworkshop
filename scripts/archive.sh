d3cac31c2f697df05b6383cee7e6e4f79487b667
Steven Hugg <hugg@fasterlight.com>
2021-02-21 13:31:24 -0600

    updated archive.sh script

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -2,16 +2,13 @@
 # build archives
 export VERSION=`git tag | tail -1`
 export PREFIX=8bitworkshop-$VERSION
-echo Creating $PREFIX
-ln -fs . $PREFIX
-git-archive-all --prefix $PREFIX release/$PREFIX.zip
-zip -9r release/$PREFIX.zip $PREFIX/gen
-#git archive --prefix 8bitworkshop- -o release/8bitworkshop-tools.zip HEAD tools
+echo "Run make dist first!"
+mv tmp/dist tmp/$PREFIX
+cd tmp
+zip -9rf ../release/$PREFIX.zip $PREFIX
+cd ..
 
 # copy to remote
-. ./scripts/env.sh
 DESTPATH=$RSYNC_PATH/release/
+echo "Copy to $DESTPATH"
 rsync -rv --progress --stats -e "ssh" release/*.zip $DESTPATH
-
-# remove symlink
-rm $PREFIX
