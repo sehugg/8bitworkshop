8fc1e92d213d69e48e226dcca705556be724188a
Fred Sauer <fredsa@google.com>
2026-03-01 19:21:42 -0800

    fix asset editor replace with different length text

--- src/ide/pixeleditor.ts
+++ src/ide/pixeleditor.ts
@@ -490,6 +490,7 @@ export class TextDataNode extends CodeProjectDataNode {
     var datastr = this.text.substring(this.start, this.end);
     datastr = replaceHexWords(datastr, this.words);
     this.text = this.text.substring(0, this.start) + datastr + this.text.substring(this.end);
+    this.end = this.start + datastr.length;
     if (this.project) {
       this.project.updateFile(this.fileid, this.text);
       //this.project.replaceTextRange(this.fileid, this.start, this.end, datastr);
