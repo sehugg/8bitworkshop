9f28f39257e2551c0c05141cbfdca647eccb4bb4
Fred Sauer <fredsa@google.com>
2026-03-04 14:51:14 -0800

    reformat src/ide/windows.ts

--- src/ide/windows.ts
+++ src/ide/windows.ts
@@ -5,46 +5,46 @@ import { WorkerError, FileData } from "../common/workertypes";
 import { getFilenamePrefix, getFilenameForPath } from "../common/util";
 import { ProjectView } from "./views/baseviews";
 
-type WindowCreateFunction = (id:string) => ProjectView;
-type WindowShowFunction = (id:string, view:ProjectView) => void;
+type WindowCreateFunction = (id: string) => ProjectView;
+type WindowShowFunction = (id: string, view: ProjectView) => void;
 
 export class ProjectWindows {
-  containerdiv : HTMLElement;
-  project : CodeProject;
-  id2window : {[id:string]:ProjectView} = {};
-  id2createfn : {[id:string]:WindowCreateFunction} = {};
-  id2showfn : {[id:string]:WindowShowFunction} = {};
-  id2div : {[id:string]:HTMLElement} = {};
-  activeid : string;
-  activewnd : ProjectView;
-  activediv : HTMLElement;
-  lasterrors : WorkerError[];
-  undofiles : string[];
-  titlePrefix : string;
+  containerdiv: HTMLElement;
+  project: CodeProject;
+  id2window: { [id: string]: ProjectView } = {};
+  id2createfn: { [id: string]: WindowCreateFunction } = {};
+  id2showfn: { [id: string]: WindowShowFunction } = {};
+  id2div: { [id: string]: HTMLElement } = {};
+  activeid: string;
+  activewnd: ProjectView;
+  activediv: HTMLElement;
+  lasterrors: WorkerError[];
+  undofiles: string[];
+  titlePrefix: string;
 
-  constructor(containerdiv:HTMLElement, project:CodeProject) {
+  constructor(containerdiv: HTMLElement, project: CodeProject) {
     this.containerdiv = containerdiv;
     this.project = project;
     this.undofiles = [];
   }
   // TODO: delete windows ever?
 
-  isWindow(id:string) : boolean {
+  isWindow(id: string): boolean {
     return this.id2createfn[id] != null;
   }
 
-  setCreateFunc(id:string, createfn:WindowCreateFunction) : void {
+  setCreateFunc(id: string, createfn: WindowCreateFunction): void {
     this.id2createfn[id] = createfn;
   }
 
-  setShowFunc(id:string, showfn:WindowShowFunction) : void {
+  setShowFunc(id: string, showfn: WindowShowFunction): void {
     this.id2showfn[id] = showfn;
   }
 
-  create(id:string) : ProjectView {
+  create(id: string): ProjectView {
     var wnd = this.id2window[id];
     if (!wnd) {
-      console.log("creating window",id);
+      console.log("creating window", id);
       wnd = this.id2window[id] = this.id2createfn[id](id);
     }
     var div = this.id2div[id];
@@ -55,7 +55,7 @@ export class ProjectWindows {
     return wnd;
   }
 
-  createOrShow(id: string, moveCursor?: boolean) : ProjectView {
+  createOrShow(id: string, moveCursor?: boolean): ProjectView {
     var wnd = this.create(id);
     var div = this.id2div[id];
     if (this.activewnd != wnd) {
@@ -89,7 +89,7 @@ export class ProjectWindows {
     return wnd;
   }
 
-  updateTitle(id: string) : void {
+  updateTitle(id: string): void {
     if (!this.titlePrefix) return;
     var mainName = getFilenameForPath(this.project.mainPath);
     if (id === this.project.mainPath) {
@@ -100,27 +100,27 @@ export class ProjectWindows {
     }
   }
 
-  put(id:string, window:ProjectView) : void {
+  put(id: string, window: ProjectView): void {
     this.id2window[id] = window;
   }
 
-  refresh(moveCursor:boolean) : void {
+  refresh(moveCursor: boolean): void {
     // refresh current window
     if (this.activewnd && this.activewnd.refresh)
       this.activewnd.refresh(moveCursor);
   }
 
-  tick() : void {
+  tick(): void {
     if (this.activewnd && this.activewnd.tick)
       this.activewnd.tick();
   }
 
-  setErrors(errors:WorkerError[]) : void {
+  setErrors(errors: WorkerError[]): void {
     this.lasterrors = errors;
     this.refreshErrors();
   }
 
-  refreshErrors() : void {
+  refreshErrors(): void {
     if (this.activewnd && this.activewnd.markErrors) {
       if (this.lasterrors && this.lasterrors.length)
         this.activewnd.markErrors(this.lasterrors);
@@ -129,18 +129,18 @@ export class ProjectWindows {
     }
   }
 
-  getActive() : ProjectView { return this.activewnd; }
+  getActive(): ProjectView { return this.activewnd; }
 
-  getActiveID() : string { return this.activeid; }
+  getActiveID(): string { return this.activeid; }
 
-  getCurrentText() : string {
+  getCurrentText(): string {
     if (this.activewnd && this.activewnd.getValue)
       return this.activewnd.getValue();
     else
       bootbox.alert("Please switch to an editor window.");
   }
 
-  resize() : void {
+  resize(): void {
     if (this.activeid && this.activewnd && this.activewnd.recreateOnResize) {
       this.activewnd = null;
       this.id2window[this.activeid] = null;
@@ -149,7 +149,7 @@ export class ProjectWindows {
     }
   }
 
-  updateFile(fileid:string, data:FileData) {
+  updateFile(fileid: string, data: FileData) {
     // is there an editor? if so, use it
     var wnd = this.id2window[fileid];
     if (wnd && wnd.setText && typeof data === 'string') {
@@ -181,11 +181,11 @@ export class ProjectWindows {
     }
   }
 
-  findWindowWithFilePrefix(filename : string) : string {
+  findWindowWithFilePrefix(filename: string): string {
     filename = getFilenameForPath(getFilenamePrefix(filename));
     for (var fileid in this.id2createfn) {
       // ignore include files (TODO)
-      if (fileid.toLowerCase().endsWith('.h') || fileid.toLowerCase().endsWith('.inc')  || fileid.toLowerCase().endsWith('.bas'))
+      if (fileid.toLowerCase().endsWith('.h') || fileid.toLowerCase().endsWith('.inc') || fileid.toLowerCase().endsWith('.bas'))
         continue;
       if (getFilenameForPath(getFilenamePrefix(fileid)) == filename) return fileid;
     }
