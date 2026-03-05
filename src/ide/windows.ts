ef2f6fcdb5c86842cb768e52d0b1dd8c4f4f6870
Fred Sauer <fredsa@google.com>
2026-03-05 15:41:09 -0800

    Fix `wnd` undefined if asset source unopened
    Fix broken asset edit and undo when the asset
    source file has not been opened manually, for
    example when reloading the #asseteditor page
    directly.

--- src/ide/windows.ts
+++ src/ide/windows.ts
@@ -165,7 +165,7 @@ export class ProjectWindows {
   }
 
   replaceTextRange(fileid: string, from: number, to: number, text: string) {
-    var wnd = this.id2window[fileid];
+    var wnd = this.id2window[fileid] || this.create(fileid);
     wnd.replaceTextRange(from, to, text);
     this.undofiles.push(fileid);
     this.redofiles = [];
