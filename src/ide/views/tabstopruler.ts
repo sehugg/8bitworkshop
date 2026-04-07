d91a159b1b675ec0d6351201086badce8e4aa42b
Fred Sauer <fredsa@google.com>
2026-04-06 20:14:57 -0700

    Clean up tabstopruler.ts

--- src/ide/views/tabstopruler.ts
+++ src/ide/views/tabstopruler.ts
@@ -1,7 +1,7 @@
 import { indentUnit } from "@codemirror/language";
 import { EditorState } from "@codemirror/state";
-import { EditorView, Panel, showPanel } from "@codemirror/view";
-import { AsmTabStops, tabStopsEquals } from "../../common/tabdetect";
+import { EditorView, Panel, ViewUpdate, showPanel } from "@codemirror/view";
+import { AsmTabStops } from "../../common/tabdetect";
 import { openSettings, tabStopsFacet } from "../settings";
 import { MAX_COLS, tabStopsToColumns } from "../views/tabs";
 
@@ -18,40 +18,42 @@ function rulerPanel(view: EditorView): Panel {
   const dom = document.createElement("div");
   dom.className = "tab-stop-ruler";
   dom.setAttribute("aria-hidden", "true");
-  let currentAsmTabStops: AsmTabStops = {};
-  let currentTabSize = 0;
-  let currentIndent = "";
 
-  function rebuild() {
+  function rebuild(update?: ViewUpdate) {
+    if (update
+      && update.startState.facet(tabStopsFacet) === update.state.facet(tabStopsFacet)
+      && update.startState.facet(EditorState.tabSize) === update.state.facet(EditorState.tabSize)
+      && update.startState.facet(indentUnit) === update.state.facet(indentUnit)) return;
     const asmTabStops = view.state.facet(tabStopsFacet);
     const tabSize = view.state.facet(EditorState.tabSize);
     const indent = view.state.facet(indentUnit);
-    if (tabStopsEquals(asmTabStops, currentAsmTabStops) && tabSize === currentTabSize && indent === currentIndent) return;
-    currentAsmTabStops = asmTabStops;
-    currentTabSize = tabSize;
-    currentIndent = indent;
     dom.innerHTML = buildContent(asmTabStops, tabSize, indent === '\t');
   }
 
-  function sync() {
+  function resize() {
     const contentStyle = getComputedStyle(view.contentDOM);
     dom.style.font = contentStyle.font;
     const left = view.contentDOM.getBoundingClientRect().left - view.dom.getBoundingClientRect().left;
     dom.style.marginLeft = left + "px";
     const line = view.contentDOM.querySelector(".cm-line");
-    if (line) dom.style.paddingLeft = getComputedStyle(line).paddingLeft;
+    dom.style.paddingLeft = line ? getComputedStyle(line).paddingLeft : "0";
   }
 
   rebuild();
+  resize();
+
+  // Removed in destroy().
   dom.addEventListener("click", openSettings);
-  sync();
 
   return {
     dom,
     top: true,
-    update() {
-      rebuild();
-      sync();
+    update(update: ViewUpdate) {
+      rebuild(update);
+      if (update.geometryChanged) resize();
+    },
+    destroy() {
+      dom.removeEventListener("click", openSettings);
     }
   };
 }
