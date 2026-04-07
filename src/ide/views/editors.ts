435ed27f48c6f6695ad303823d9b4f8572801a77
Fred Sauer <fredsa@google.com>
2026-04-06 20:19:24 -0700

    Remove unused setCurrentLine return value
    Previously returned boolean | undefined

--- src/ide/views/editors.ts
+++ src/ide/views/editors.ts
@@ -562,7 +562,7 @@ export class SourceEditor implements ProjectView {
     if (line) {
       // Validate line number is within document range (TODO: open disassembler)
       if (line.line < 1 || line.line > this.editor.state.doc.lines) {
-        return false;
+        return;
       }
       addCurrentMarker(line);
       if (moveCursor) {
@@ -574,7 +574,6 @@ export class SourceEditor implements ProjectView {
         });
       }
       this.currentDebugLine = line;
-      return true;
     }
   }
 
