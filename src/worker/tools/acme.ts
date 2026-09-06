66376e59009ef62035a17d947494ac998f4bcb02
Steven Hugg <hugg@fasterlight.com>
2026-09-06 09:50:59 +0200

    worker: converted acme/zmac to WASI

--- src/worker/tools/acme.ts
+++ src/worker/tools/acme.ts
@@ -1,7 +1,8 @@
-import { CodeListing, CodeListingMap } from "../../common/workertypes";
-import { BuildStep, BuildStepResult, gatherFiles, staleFiles, populateFiles, fixParamsWithDefines, putWorkFile } from "../builder";
-import { msvcErrorMatcher } from "../listingutils";
-import { loadNative, moduleInstFn, print_fn, setupFS, execMain, emglobal, EmscriptenModule } from "../wasmutils";
+import { CodeListing, CodeListingMap, WorkerError } from "../../common/workertypes";
+import { BuildStep, BuildStepResult, gatherFiles, staleFiles, putWorkFile, store } from "../builder";
+import { msvcErrorMatcher, re_crlf } from "../listingutils";
+import { WASIRunner } from "../../common/wasi/wasishim";
+import { loadWASMBinary } from "../wasmutils";
 
 function parseACMESymbolTable(text: string) {
     var symbolmap = {};
@@ -47,25 +48,24 @@ function parseACMEReportFile(text: string) {
     return listings;
 }
 
+let wasiModule: WebAssembly.Module | null = null;
+
 export function assembleACME(step: BuildStep): BuildStepResult {
-    loadNative("acme");
-    var errors = [];
+    let errors: WorkerError[] = [];
     gatherFiles(step, { mainFilePath: "main.acme" });
     var binpath = step.prefix + ".bin";
     var lstpath = step.prefix + ".lst";
     var sympath = step.prefix + ".sym";
     if (staleFiles(step, [binpath])) {
-        var binout, lstout, symout;
-        var ACME: EmscriptenModule = emglobal.acme({
-            instantiateWasm: moduleInstFn('acme'),
-            noInitialRun: true,
-            print: print_fn,
-            printErr: msvcErrorMatcher(errors),
-            //printErr: makeErrorMatcher(errors, /(Error|Warning) - File (.+?), line (\d+)[^:]+: (.+)/, 3, 4, step.path, 2),
-        });
-        var FS = ACME.FS;
-        populateFiles(step, FS);
-        fixParamsWithDefines(step.path, step.params);
+        if (!wasiModule) {
+            wasiModule = new WebAssembly.Module(loadWASMBinary("acme"));
+        }
+        const wasi = new WASIRunner();
+        wasi.initSync(wasiModule);
+        for (let file of step.files) {
+            wasi.fs.putFile("./" + file, store.getFileData(file));
+        }
+        wasi.addPreopenDirectory(".");
         var args = ['--msvc', '--initmem', '0', '-o', binpath, '-r', lstpath, '-l', sympath, step.path];
         if (step.params?.acmeargs) {
             args.unshift.apply(args, step.params.acmeargs);
@@ -76,14 +76,32 @@ export function assembleACME(step: BuildStep): BuildStepResult {
         if (step.mainfile) {
             args.unshift.apply(args, ["-D__MAIN__=1"]);
         }
-        execMain(step, ACME, args);
+        wasi.setArgs(['acme', ...args]);
+        try {
+            wasi.run();
+        } catch (e) {
+            errors.push({ line: 0, msg: "" + e });
+        }
+        const stdout = wasi.fds[1].getBytesAsString();
+        const stderr = wasi.fds[2].getBytesAsString();
+        if (stdout) console.log(stdout);
+        const matcher = msvcErrorMatcher(errors);
+        for (let line of stderr.split(re_crlf)) {
+            matcher(line);
+        }
         if (errors.length) {
             let listings: CodeListingMap = {};
             return { errors, listings };
         }
-        binout = FS.readFile(binpath, { encoding: 'binary' });
-        lstout = FS.readFile(lstpath, { encoding: 'utf8' });
-        symout = FS.readFile(sympath, { encoding: 'utf8' });
+        let binout: Uint8Array, lstout: string, symout: string;
+        try {
+            binout = wasi.fs.getFile("./" + binpath).getBytes();
+            lstout = wasi.fs.getFile("./" + lstpath).getBytesAsString();
+            symout = wasi.fs.getFile("./" + sympath).getBytesAsString();
+        } catch (e) {
+            errors.push({ line: 0, msg: "No output generated, maybe a fatal assembly error?" });
+            return { errors };
+        }
         putWorkFile(binpath, binout);
         putWorkFile(lstpath, lstout);
         putWorkFile(sympath, symout);
